7a1473359e97b71ffcf303d1b310a8b7736f84dc
Nicolas Bisio <nb.bisio@gmail.com>
2022-10-31 12:34:02 -0300

    Tercera Pre-entrega JS

--- js/checkout.js
+++ js/checkout.js
@@ -1,15 +1,29 @@
-const mostrarCarrito = () => {
+const recuperarCarrito = () => {
+    if (localStorage.getItem("carrito")) {
+        let carritoRecuperado = JSON.parse(localStorage.getItem("carrito"))
+        carritoRecuperado.forEach(producto => carrito.push(producto))
+    }
+    else {
+        console.warn("No se encontró un carrito previamente guardado.")
+    }
+}
+
+const mostrarCarrito = ()=> {
     let tablaCheckout = ""
     const checkout__tbody = document.querySelector("tbody.checkout__tbody")
     carrito.forEach(producto => {
-        tablaCheckout += `<tr>
-                            <td></td>
+        tablaCheckout += `<tr class="checkout__td">
+                            <td class="checkout__td"><img src="./img/${producto.imagen}.jpg" alt="${producto.imagen}" class="checkout__imagen"></td>
                             <td>${producto.nombre}</td>
-                            <td>${producto.precio}</td>
+                            <td class="checkout__td">$ ${producto.precio}</td>
+                            <td class="checkout__td"><img src="./img/tacho.png" alt="tacho" class="checkout__tacho"></td>
                         </tr>`
     })
     checkout__tbody.innerHTML = tablaCheckout
 }
 
-/* //SE MUESTRAN LOS PRODUCTOS EN LA TABLA DE CHECKOUT.HTML
-mostrarCarrito() */
+//SE RECUPERA EL CARRITO AL CARGAR INDEX.HTML
+recuperarCarrito()
+
+//SE MUESTRAN LOS PRODUCTOS EN LA TABLA DE CHECKOUT.HTML
+mostrarCarrito()
--- js/templates.js
+++ js/templates.js
@@ -1,10 +1,10 @@
-const retornoCard = (producto) => {
+const retornoCard = ({imagen, nombre, precio}) => {
     return `<div class="card">
-                <div class=""><img src="./img/${producto.imagen}.jpg" alt="${producto.imagen}" class="card__imagen"></div>
-                <div class="card-name">${producto.nombre}</div>
-                <div class="card-price">$ ${producto.precio}</div>
+                <div class=""><img src="./img/${imagen}.jpg" alt="${imagen}" class="card__imagen"></div>
+                <div class="card-name">${nombre}</div>
+                <div class="card-price">$ ${precio}</div>
                 <div class="boton card-button">
-                    <button class="button button-outline button-add" id="${producto.nombre}" title="Pulsa para agregar '${producto.nombre}' al carrito">+</button>
+                    <button class="button button-outline button-add" id="${nombre}" title="Pulsa para agregar '${nombre}' al carrito">+</button>
                 </div>
             </div>`
 
